e0632842939b267177f66e2c2fd50ff2a10a9d98
LiteObject <Lite.Object@gmail.com>
2022-02-16 22:03:46 -0600

    Initial solution setup with greeter service

---
+++ Demo.Grpc.Client/Program.cs
@@ -0,0 +1,9 @@
+﻿using Demo.Grpc.Service;
+using Grpc.Net.Client;
+
+var channel = GrpcChannel.ForAddress("https://localhost:7052");
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(
+       new Demo.Grpc.Service.HelloRequest { Name = "gRPC Demo" });
+
+Console.WriteLine("from server: " + reply);
